82624dc46a609c9f68a900b94bcb0ba55a5fbd1a
pompucha <benavidesga@gmail.com>
2019-05-29 09:16:47 -0600

    intial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var options = ["rock,", "paper", "scissor"];
+
+var img = document.getElementById('img')
+var userChoice;
+var compChoice;
+
+var choices = document.getElementsByIdClassName("choice");
+console.log(choices);
+// for (var i = 0; i < choices.length; i++) {
+//     choice[i].addEventListener("click", function() {
+//         userChoice = this.id;
+//         // get comp choice
+//         //display choices
+//         // figure out winner
+//     });
+// }
+
+document.getElementById( "Img" ).onclick = function() {
+    // img clicked
+};
